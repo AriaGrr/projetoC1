1478d3f7829943fe7f11aa7ac7c727b2ecb3e5e4
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2023-09-15 17:42:43 -0300

    commit aula

---
+++ proj.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+    int prioridade;
+    char categoria[100];
+    char descricao[100];
+} Tarefa;
+
+typedef struct {
+
+} ListaDeTarefas;
+
+int main (){
+
+}

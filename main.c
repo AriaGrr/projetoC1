0736ec720a6b96f4af2c02137fcfe68e3da1140e
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2023-09-16 21:09:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,33 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include "funcoes.h"
 
-
+#ifdef _WIN32 // Verifica se é Windows
+    #define CLEAR_SCREEN "cls"
+    #define PAUSE "pause"
+#else
+    #define CLEAR_SCREEN "clear"
+    #define PAUSE "read -p 'Pressione Enter para continuar...' -n1 -s"
+#endif
 
 int main (){
+    setlocale(LC_ALL, "Portuguese");
+
     int cod;
     char arquivo[] = "tarefas";
     ListaDeTarefas lt;
 
     cod = carregarLista(&lt, arquivo);
-    if (cod == 1)
+    if (cod == 1) {
         lt.qtd = 0;
-
+    }
     int opcao;
-    do{
+    do{    
         printMenu();
         scanf("%d", &opcao);
         printf("%d\n", opcao);
-        if(opcao == 0)
-        ;
+        if(opcao == 0){
+            printf("Saindo...\n");
+        }
         else if (opcao == 1){
             criarTarefa(&lt);
+            // Limpar o terminal
+            system(CLEAR_SCREEN);
         } else if (opcao == 2){
             deletarTarefa(&lt);
         } else if (opcao == 3){
+            system(CLEAR_SCREEN);
             listarTarefa(lt);
+            
+            printf("Pressione Enter para voltar ao menu...");
+            getchar(); // Captura o Enter
+            getchar(); // Aguarda o próximo Enter
+
+            system(CLEAR_SCREEN);
         } else {
-            printf("Opcao não existe!\n");
+            wprintf(L"Opção não existe!\n");
         }
     } while (opcao != 0);
 
